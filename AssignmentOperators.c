bb29dd40e010d380121c04c88f2f9f9df0b18dd1
Ankit Parida <95227927+Lonely-7@users.noreply.github.com>
2022-10-23 00:02:25 +0530

    Assignment Operations in C
    So recently I've started C as per the requirement of my semester. I've been following w3schools.com for learning C and I came up with this assignment operators where I'm facing problem. I couldn't find any error in my syntax so please help me to get the satisfying output. :)

---
+++ AssignmentOperators.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  //Assignment Operators
+  int assign_1=8; //assignment(=)
+  printf("%d\n",assign_1);
+  int assign_1+=1; //addition assignment(+=)
+  printf("%d\n",assign_1);
+  int assign_1-=1; //subtraction assignment(-=)
+  printf("%d\n",assign_1);
+  int assign_1*=2; //multiplication assignment(*=)
+  printf("%d\n",assign_1);
+  int assign_1/=2; //division assignment(/=)
+  printf("%d\n",assign_1);
+  int assign_1%=2; //modulus assignment(%=)
+  printf("%d\n",assign_1);
+
+  return 0;
+}
